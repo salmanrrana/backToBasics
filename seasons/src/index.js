5506e316e4d85786d577415f1a70b800c5d9e953
Salman Rana <salman.r.rana@gmail.com>
2018-11-23 11:36:44 -0600

    added some conditional rendering to handle loading, error messages, and lat. Also this is the first way to setState. refactor on setting state coming soon

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,22 +5,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    // THis is the only time we do direct assignment to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
-        // to update our state object -> we called setState
         this.setState({ lat: position.coords.latitude });
         console.log("The state is: ", this.state.lat);
       },
-      err => console.log(err)
+      err => {
+        console.log("THis is the err: ", err);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
-  //React says we must define render()!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
