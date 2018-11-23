7743b73b69a31c249eecc2b024f67975d90f2b05
Salman Rana <salman.r.rana@gmail.com>
2018-11-23 08:45:15 -0600

    fixed jsx folder now

---
+++ jsx_test/src/index.js
@@ -0,0 +1,24 @@
+// import the react and react DOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+//Attempting to fix
+// Create a react component
+const App = () => {
+  const buttonText = { text: "Click Me!" };
+  const labelText = "Enter Name";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" tyle="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
