184801520a0dca9ef579fdc089342d56152849f3
Salman Rana <salman.r.rana@gmail.com>
2018-12-04 08:36:05 -0600

    completed video search project

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -10,13 +10,20 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount = () => {
+    this.onTermSubmit("Jeff Goldblum laugh 10 hours");
+  };
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
@@ -29,10 +36,10 @@ class App extends React.Component {
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
-            <div>
+            <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
             </div>
-            <div>
+            <div className="five wide column">
               <VideoList
                 onVideoSelect={this.onVideoSelect}
                 videos={this.state.videos}
