dfd84fbc17172d4f85709e902182c60809d9ad39
Salman Rana <salman.r.rana@gmail.com>
2018-12-14 11:26:34 -0600

    added some boilerplate for action creators

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,6 +1,15 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 
+// THis is the sign in action creator
 export const signIn = userId => {
   return {
     type: SIGN_IN,
@@ -8,12 +17,40 @@ export const signIn = userId => {
   };
 };
 
+// THis is the sign out action creator
 export const signOut = () => {
   return {
     type: SIGN_OUT
   };
 };
 
+// This is the action creator to create a stream
 export const createStream = formValues => async dispatch => {
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deteleStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
