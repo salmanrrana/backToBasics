f219f96f4bd780017b0c5fa74e605dd0970844c0
Salman Rana <salman.r.rana@gmail.com>
2018-12-15 12:02:05 -0600

    ability to render streams in StreamList component

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -31,7 +31,7 @@ export const createStream = formValues => async dispatch => {
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
-export const fetchStreams = async dispatch => {
+export const fetchStreams = () => async dispatch => {
   const response = await streams.get("/streams");
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
