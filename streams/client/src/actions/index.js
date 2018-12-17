8047578be4706f0dbc953f66be5528a2beba125b
Salman Rana <salman.r.rana@gmail.com>
2018-12-17 12:29:00 -0600

    added ability to see if a user is signedin  and attach userId to a new Stream. Also added link to create stream screen

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -25,8 +25,9 @@ export const signOut = () => {
 };
 
 // This is the action creator to create a stream
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
