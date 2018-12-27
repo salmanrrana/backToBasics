0e3a0e32a4c625837b579d39740c22a987d5d5e7
Salman Rana <salman.r.rana@gmail.com>
2018-12-27 14:26:45 -0600

    added ability to delete stream and navigate back to the home page

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -56,8 +56,9 @@ export const editStream = (id, formValues) => async dispatch => {
   history.push("/");
 };
 
-export const deteleStream = id => async dispatch => {
+export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
 };
