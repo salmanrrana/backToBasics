4f5748a961bf710041319508acc0c5eec6104c13
Salman Rana <salman.r.rana@gmail.com>
2018-12-27 17:49:08 -0600

    created some boilerplate for the translate app that will be showcasing how context works in react

---
+++ translate/src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag pk"
+            onClick={() => this.onLanguageChange("urdu")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
