46e6e08d0dd5d08c88f9fd807682a8d21b84bbd4
Salman Rana <salman.r.rana@gmail.com>
2018-12-29 17:48:37 -0600

    added memoize from lodash to help solve the issue of the many calls to the API

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,16 +1,5 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-
-//bad approach !!!!!!
-// get error message: ×
-// Error: Actions must be plain objects. Use custom middleware for async actions.
-// export const fetchPosts = async () => {
-//   const response = await jsonPlaceholder.get("/posts");
-//
-//   return {
-//     type: "FETCH_POSTS",
-//     payload: response
-//   };
-// };
 
 // export const fetchPosts = async () => {
 //   return async dispatch => {
@@ -32,8 +21,12 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
