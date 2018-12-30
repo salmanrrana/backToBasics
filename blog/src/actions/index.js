4fd12b01ebc2cd0279d13a9424b2c8bf19b042fa
Salman Rana <salman.r.rana@gmail.com>
2018-12-29 18:27:44 -0600

    using uniq, map, chain from lodash for alternative solutions to the multiple API call issue

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,32 +1,38 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-// export const fetchPosts = async () => {
-//   return async dispatch => {
-//     const response = await jsonPlaceholder.get("/posts");
-//
-//     dispatch({
-//       type: "FETCH_POSTS",
-//       payload: response
-//     });
-//   };
-// };
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
 
-// This is the same code as the one directly above
-// however it has been refactored down. this may be  the normal
-// type of way we may see it in documentation
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// The following two functions is the
+// memoized version of the approach to
+// solve the multiple API calls
+
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
